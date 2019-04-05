12f8b9af1d20982ef052e431a44224e104ed8d65
Nicola <nicola2.melas@gmail.com>
2019-04-05 15:53:35 +0200

    map_exercise fixed

--- map_exercise.cpp
+++ map_exercise.cpp
@@ -12,16 +12,18 @@ main ()
   std::uniform_int_distribution<int> intdist (0, 100);
   std::uniform_real_distribution<double> doubdist (0.0, 1.0);
 
-  for (int ii = 0; ii < 10; ++ii)
+  for (int ii = 0; ii < 20; ++ii)
     m[intdist (generator)] = doubdist (generator);
 
   std::cout << "initial contents" << std::endl;
   for (auto& jj : m)
     std::cout << jj.first << " " << jj.second << std::endl;
 
-  for (auto jj = m.begin (); jj != m.end (); ++jj)
-    if (jj->second < .5)
-      m.erase (jj);
+  for (auto jj = m.begin (); jj != m.end ();)
+    if (jj->second < .5) 
+    	  jj = m.erase (jj);
+    else	  
+    	  jj++;
 
   std::cout << "final contents" << std::endl;
   for (auto& jj : m)
